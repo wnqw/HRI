0789560d732c8a0a171af48f667ab8fdd44f7f4b
WenqingWang <wnqwang@ucdavis.edu>
2023-04-26 01:40:41 -0400

    working

--- scripts/ai_robot.js
+++ scripts/ai_robot.js
@@ -5,6 +5,12 @@ const dirToMovement = {
     right: [0, 1],
     wait: [0, 0]
 }
+const keyToInd = {
+    id: 0,
+    loc: 1,
+    dir: 2,
+    holding: 3 // not used yet
+}
 
 // ROBOT LEVEL
 let robot_bfs_level = 1;
@@ -22,158 +28,8 @@ let curLowPlan = [];
 // others
 let level0robot_policy = 0; 
 
-function setHighPlan(playerID, gameState){
-    let agentInd = - 1; 
-    
-    for(let i = 0; i < gameState.agents.length;i++){
-        if(gameState.agents[i].id === playerID){
-            agentInd = i;
-        }
-    }
-    if(agentInd === -1) return;
-
-    curHighPlan_Robot_level0 = gameState.policy[level0robot_policy];
-    curHighPlan_Robot_level1.plans = gameState.all_locs;
-    curHighPlan_Robot_level2.plans = gameState.all_locs;
-    curHighPlan_Human_level1.plans = gameState.all_locs;
-    curHighPlan_Human_level2.plans = gameState.all_locs;
-
-    if (robot_bfs_level === 0){
-        console.log('ROBOT LEVEL = 0');
-        console.log('initial curHighPlan_Robot_level0: ' + curHighPlan_Robot_level0);
-    }
-    else if (robot_bfs_level === 1){
-        console.log('ROBOT LEVEL = 1');
-    }
-    else if (robot_bfs_level === 2){
-        console.log('ROBOT LEVEL = 2');
-    }
- }
-
-
-function nextAction(playerID, gameState){
-    if(curHighPlan_Robot_level0.length===0 && curLowPlan.length ===0) return;
-
-    if(curLowPlan.length === 0){
-
-        if (robot_bfs_level === 0){
-            curLowPlan = bfs_level0(playerID, gameState, tar_loc = findLocation(curHighPlan_Robot_level0[0], gameState.map), undefined);
-        }
-        else if (robot_bfs_level === 1){
-            curLowPlan = bfs_level1(playerID, "1", gameState, undefined, undefined, curHighPlan_Robot_level1, curHighPlan_Human_level1);
-        }
-        else if (robot_bfs_level === 2){
-            curLowPlan = bfs_level2(playerID, "1", gameState, curHighPlan_Robot_level2, curHighPlan_Human_level2, curHighPlan_Robot_level1, curHighPlan_Human_level1);
-        }
-        
-        if(curLowPlan.length != 0){
-            curLowPlan.push("interact");
-            if (robot_bfs_level === 0){
-                curHighPlan_Robot_level0.shift();
-            }
-        }
-    }
-    
-    const next_action = curLowPlan.shift();
-
-    executeAction(playerID, gameState, next_action);
-}
-
-
-function manhattanDistance(loc1, loc2){
-    return(Math.abs(loc1[0] - loc2[0]) + Math.abs(loc1[1] - loc2[1]));
-}
-
-//Simplified State Space 
-//Necesarity elements
-// My location, The other agent location and their directions. That's all. 
-// The (new) map system allow you to pick and drop from the box locations and they won't be gone
-// So, you don't need to maintain their status anymore. 
-// SeearchState = all agents status (id, loc, dir, hold)
-
-function findIndfromID(list, id){
-    for(let i = 0; i< list.length;i++){
-        if(list[i][keyToInd.id] === id){
-            return(i);
-        }
-    }
-    return(-1);
-}
-
-function nextState(searchNode, action, target_id, gameState){
-    //Generate the next search state
-    //Only care about the next location(s)
-    //TODO: generalize to multiple agents
-    
-    const newSearchNode = structuredClone(searchNode); //Is there a better way to deep clone this?
-
-    //TODO: loop through a list of id for multiple actions
-    const ind = findIndfromID(newSearchNode.agents, target_id); 
-    const player = newSearchNode.agents[ind]; 
-    
-    if(action === player[keyToInd.dir]){
-        //Move to the next location if possible 
-        let new_loc = [player[keyToInd.loc][0], player[keyToInd.loc][1]];
-        new_loc[0] = new_loc[0] + dirToMovement[action][0];
-        new_loc[1] = new_loc[1] + dirToMovement[action][1];
-
-        new_loc_ele = gameState.map[new_loc[0]][new_loc[1]];
-        if(new_loc_ele === "." || new_loc_ele === target_id){
-            player[keyToInd.loc] = new_loc;
-        }
-    }
-    else if(action === control.wait){
-        //do nothing
-    }
-    else if(action === control.interact){
-        //place holder for now
-    }else{
-        //if action != current direction
-        player[keyToInd.dir] = action;
-    }
-    
-    newSearchNode.hist.push(action);
-    return(newSearchNode);
-}
-
-function checkAgentDuped(a1, a2){
-    return((a1[1][0] === a2[1][0]) && (a1[1][1] === a2[1][1]) && (a1[2] === a2[2]))
-}
-
-function checkDupedSimpState(states, newState){
-    const newAgents = newState.agents;
-    for(const s of states){
-        let duped = true;
-        for(let i =0; i < s.agents.length; i++){
-            duped = duped && checkAgentDuped(s.agents[i],newAgents[i]);
-        }
-        if(duped) {
-            return(duped);
-        }
-    }
-    return(false);
-}
-
-const keyToInd = {
-    id: 0,
-    loc: 1,
-    dir: 2,
-    holding: 3 // not used yet
-}
-
-function checkGoalLocation(player, tar_loc){
-    //Input: simplified player 
-    //If adjacent and dir = point to the target, true
-    const temp_dir = dirToMovement[player[keyToInd.dir]];
-    const temp_loc = player[keyToInd.loc];
-    if( (temp_loc[0] + temp_dir[0]) === tar_loc[0] && 
-        (temp_loc[1] + temp_dir[1]) === tar_loc[1]){
-            return(true);
-    }
-    return(false);
-}
-
 const actions_list = [control.up, control.down, control.left, control.right, control.wait];
+
 
 function bfs_level0(agentID, gameState, tar_loc, searchNode){
     /*
@@ -395,11 +251,10 @@ function bfs_level2(agentID, otherID, gameState, player_objIDs, other_objIDs, pl
 
 
 
-// robot find obj nearest to human
-// interact with obj: if human cant do, and robot can do 
-// if not interact: robot find next nearest obj
-
 function agent_response_action_dynamic(agentID, otherID, others_action, gameState, nextNode, player_objIDs, other_objIDs){
+    // robot find obj nearest to human
+    // interact with obj: if human cant do, and robot can do 
+    // if not interact: robot find next nearest obj
 
     let other_player = nextNode.agents[findIndfromID(nextNode.agents, otherID)];
     let player = nextNode.agents[findIndfromID(nextNode.agents, agentID)];
@@ -490,8 +345,7 @@ function find_deliver_place(player_instance, gameState){
 }
 
 
-
-
+// helper functions
 
 function findObjfromID(gamegState, id){
     for(const obj of gamegState.objs){
@@ -580,4 +434,150 @@ function find_next_nearest_target_id_to_player(gameState, player_loc, locs){
     }
 
     return player_distance_plan[1][1];
+}
+
+function checkGoalLocation(player, tar_loc){
+    //Input: simplified player 
+    //If adjacent and dir = point to the target, true
+    const temp_dir = dirToMovement[player[keyToInd.dir]];
+    const temp_loc = player[keyToInd.loc];
+    if( (temp_loc[0] + temp_dir[0]) === tar_loc[0] && 
+        (temp_loc[1] + temp_dir[1]) === tar_loc[1]){
+            return(true);
+    }
+    return(false);
+}
+
+
+
+function setHighPlan(playerID, gameState){
+    let agentInd = - 1; 
+    
+    for(let i = 0; i < gameState.agents.length;i++){
+        if(gameState.agents[i].id === playerID){
+            agentInd = i;
+        }
+    }
+    if(agentInd === -1) return;
+
+    curHighPlan_Robot_level0 = gameState.policy[level0robot_policy];
+    curHighPlan_Robot_level1.plans = gameState.all_locs;
+    curHighPlan_Robot_level2.plans = gameState.all_locs;
+    curHighPlan_Human_level1.plans = gameState.all_locs;
+    curHighPlan_Human_level2.plans = gameState.all_locs;
+
+    if (robot_bfs_level === 0){
+        console.log('ROBOT LEVEL = 0');
+        console.log('initial curHighPlan_Robot_level0: ' + curHighPlan_Robot_level0);
+    }
+    else if (robot_bfs_level === 1){
+        console.log('ROBOT LEVEL = 1');
+    }
+    else if (robot_bfs_level === 2){
+        console.log('ROBOT LEVEL = 2');
+    }
+ }
+
+
+function nextAction(playerID, gameState){
+    if(curHighPlan_Robot_level0.length===0 && curLowPlan.length ===0) return;
+
+    if(curLowPlan.length === 0){
+
+        if (robot_bfs_level === 0){
+            curLowPlan = bfs_level0(playerID, gameState, tar_loc = findLocation(curHighPlan_Robot_level0[0], gameState.map), undefined);
+        }
+        else if (robot_bfs_level === 1){
+            curLowPlan = bfs_level1(playerID, "1", gameState, undefined, undefined, curHighPlan_Robot_level1, curHighPlan_Human_level1);
+        }
+        else if (robot_bfs_level === 2){
+            curLowPlan = bfs_level2(playerID, "1", gameState, curHighPlan_Robot_level2, curHighPlan_Human_level2, curHighPlan_Robot_level1, curHighPlan_Human_level1);
+        }
+        
+        if(curLowPlan.length != 0){
+            curLowPlan.push("interact");
+            if (robot_bfs_level === 0){
+                curHighPlan_Robot_level0.shift();
+            }
+        }
+    }
+    
+    const next_action = curLowPlan.shift();
+
+    executeAction(playerID, gameState, next_action);
+}
+
+
+function manhattanDistance(loc1, loc2){
+    return(Math.abs(loc1[0] - loc2[0]) + Math.abs(loc1[1] - loc2[1]));
+}
+
+//Simplified State Space 
+//Necesarity elements
+// My location, The other agent location and their directions. That's all. 
+// The (new) map system allow you to pick and drop from the box locations and they won't be gone
+// So, you don't need to maintain their status anymore. 
+// SeearchState = all agents status (id, loc, dir, hold)
+
+function findIndfromID(list, id){
+    for(let i = 0; i< list.length;i++){
+        if(list[i][keyToInd.id] === id){
+            return(i);
+        }
+    }
+    return(-1);
+}
+
+function nextState(searchNode, action, target_id, gameState){
+    //Generate the next search state
+    //Only care about the next location(s)
+    //TODO: generalize to multiple agents
+    
+    const newSearchNode = structuredClone(searchNode); //Is there a better way to deep clone this?
+
+    //TODO: loop through a list of id for multiple actions
+    const ind = findIndfromID(newSearchNode.agents, target_id); 
+    const player = newSearchNode.agents[ind]; 
+    
+    if(action === player[keyToInd.dir]){
+        //Move to the next location if possible 
+        let new_loc = [player[keyToInd.loc][0], player[keyToInd.loc][1]];
+        new_loc[0] = new_loc[0] + dirToMovement[action][0];
+        new_loc[1] = new_loc[1] + dirToMovement[action][1];
+
+        new_loc_ele = gameState.map[new_loc[0]][new_loc[1]];
+        if(new_loc_ele === "." || new_loc_ele === target_id){
+            player[keyToInd.loc] = new_loc;
+        }
+    }
+    else if(action === control.wait){
+        //do nothing
+    }
+    else if(action === control.interact){
+        //place holder for now
+    }else{
+        //if action != current direction
+        player[keyToInd.dir] = action;
+    }
+    
+    newSearchNode.hist.push(action);
+    return(newSearchNode);
+}
+
+function checkAgentDuped(a1, a2){
+    return((a1[1][0] === a2[1][0]) && (a1[1][1] === a2[1][1]) && (a1[2] === a2[2]))
+}
+
+function checkDupedSimpState(states, newState){
+    const newAgents = newState.agents;
+    for(const s of states){
+        let duped = true;
+        for(let i =0; i < s.agents.length; i++){
+            duped = duped && checkAgentDuped(s.agents[i],newAgents[i]);
+        }
+        if(duped) {
+            return(duped);
+        }
+    }
+    return(false);
 }
